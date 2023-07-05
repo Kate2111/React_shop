d7ae37f654242b688295e1af5882e7509bcc928f
Kate2111 <noskova2111@mail.ru>
2023-07-05 18:21:38 +0400

    implemented the opening of the page with information about the product

--- src/components/catalog/list/CatalogList.jsx
+++ src/components/catalog/list/CatalogList.jsx
@@ -1,36 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import style from './CatalogList.module.scss';
 import buy from '@assets/images/catalog_page/Buy.svg';
 import heart from '@assets/images/catalog_page/Heart.svg';
 import {useNavigate} from 'react-router-dom';
-import { nanoid } from 'nanoid';
+import {CatalogeList} from '../../../API/context';
 
 const GalleryList = ({active}) => {
-      const catalog = [
-        { category: 'Bedroom furniture', info: [{id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_1.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_2.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_3.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_4.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_3.jpg'}] },
-        { category: 'Living room furniture', info: [{id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_1.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_2.webp'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_3.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_4.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_3.jpg'}]},
-        { category: 'Office furniture', info: [{id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_1.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_2.webp'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_3.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_4.webp'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_3.jpg'}]},
-        { category: 'Dining_room_futniture', info: [{id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_1.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_2.webp'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_3.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_4.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_5.webp'}]},
-        { category: 'Chair', info: [{id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_1.jpg'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_2.webp'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_3.webp'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_4.webp'}, {id: nanoid(5), title:'Диван "Ergonomic Rubber Shoes"', price:'35 990 ₽', image: 'img_5.webp'}]},
-      ]
+    const {catalog, setCatalog} = useContext(CatalogeList);
 
-      const navigate = useNavigate();
+    const navigate = useNavigate();
 
     return (
         <>
             { 
-                catalog.map(({ category, info}, index) => {
+                catalog.map(({category, info}, index) => {
                     if(index === active) {
                         return (<div className={`${style.item} ${style.active}`} key={category}>
                                 {
                                     info.map((elem) => {
                                         return <div className={style.wrapper} key={elem.id}>
-                                                    <img
-                                                        onClick={() => navigate(`/React_shop/catalog/${elem.id}`)}
-                                                        className={style.img}
-                                                        src={require(`@assets/images/first_page/gallery/${category}/${elem.image}`)}
-                                                        alt={category}
-                                                    />
+                                                    <a onClick={() => navigate(`/React_shop/catalog/${elem.id}`)} target='_blank'>
+                                                        <img
+                                                            className={style.img}
+                                                            src={elem.image}
+                                                            alt={category}
+                                                        />
+                                                    </a>
                                                     <div className={style.info}>
                                                         <h4 className={style.title}>{elem.title}</h4>
                                                         <div className={style.inner}>
@@ -39,10 +34,7 @@ const GalleryList = ({active}) => {
                                                                 <img  className={style.icon} src={heart} alt="heart" />
                                                                 <img className={style.icon} src={buy} alt="buy" />
                                                             </div>
-                                
-                                                            
-                                                        </div>
-                                                        
+                                                        </div>     
                                                     </div>
                                             </div> 
                                     })
--- src/pages/product_page/ProductPage.jsx
+++ src/pages/product_page/ProductPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-//import {useParams} from 'react-router-dom';
+import React, {useContext} from 'react';
+import {useParams} from 'react-router-dom';
 import style from './ProductPage.module.scss';
 import star from '@assets/images/product_page/Star.svg';
 import heart from '@assets/images/product_page/Heart.svg';
@@ -11,86 +11,96 @@ import tinkoff from '@assets/images/product_page/tinkoff.svg';
 import MyButtonTab from '@components/UI/button/MyButtonTab';
 import MyButtonYellow from '@components/UI/button/yellow_button/MyButtonYellow';
 import classNames from 'classnames';
+import {CatalogeList} from '../../API/context';
 
 
 const ProductPage = () => {
-    //const params = useParams();
-    //const index = params.id;
+    const params = useParams();
+    const index = params.id;
 
+    const {catalog, setCatalog} = useContext(CatalogeList);
 
  
     return ( <>
         <div className='container'>
-        <div className={style.wrapper}>
-                <div className={style.image}>
-                    <img     
-                        className={style.img}
-                        src={require(`@assets/images/first_page/gallery/Dining_room_futniture/img_3.jpg`)}
-                        alt='Dining_room_futniture'
-                    />
-                </div>
-                <div className={style.info}>
-                    <h1 className={style.title}>Диван "Ergonomic Rubber Shoes"</h1>
-                    <div className={classNames(style.inner, style.inner_bottom)}>
-                        <p className={style.text}>Оценка покупателей:</p>
-                        <div className={style.inner_review}>
-                            <div className={style.inner_star}>
-                                <img src={star} alt="star"/>
-                                <img src={star} alt="star"/>
-                                <img src={star} alt="star"/>
-                                <img src={star} alt="star"/>
-                                <img src={star} alt="star"/>
-                            </div>
-                            <p className={style.text}>Нет отзывов</p>
-                        </div>
-                    </div>
-                    <div className={classNames(style.inner, style.inner_bottom)}>
-                        <p className={style.text}>Код товара:</p>
-                        <p className={style.id_product}>i00078385</p>
-                    </div>
-                    <div className={classNames(style.inner, style.inner_bottom)}>
-                        <div className={classNames(style.inner_column, style.column_gap)}>
-                            <div>
-                                <p className={style.last_price}>4,700 руб</p>
-                                <p className={style.new_price}>2,700 руб</p>
-                            </div>
-                            <div className={style.pay_part}>
-                                <p className={style.text}>Оплата частями:</p>
-                                <p className={style.text}>6 х 800 руб</p>
-                            </div>
-                        </div>
-                        <div className={classNames(style.inner_column, style.column_gap)}>
-                            <div className={style.buy_buttons}>
-                                <MyButtonYellow>Купить</MyButtonYellow>
-                                <MyButtonTab><img src={heart} alt="heart" /></MyButtonTab>
-                            </div>
-                            <MyButtonTab>Купить в кредит</MyButtonTab>
-                            <div className={style.icons}>
-                                <img src={tinkoff} alt="tinkoff"/>
-                                <img src={sbp} alt="sbp"/>
-                                <img src={gazprombank} alt="gazprombank"/>
-                                <img src={raiffeisen} alt="raiffeisen"/>
-                                <img src={citibank} alt="citibank"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={style.inner_column}>
-                        <h2 className={style.subtitle}>Размер и Характеристики</h2>
-                        <div className={style.inner}>
-                            <p className={style.text}>Материал корпуса</p>
-                            <p className={style.id_product}>Металл</p>
-                        </div>
-                        <div className={style.inner}>
-                            <p className={style.text}>Изготовитель</p>
-                            <p className={style.id_product}>Украина</p>
-                        </div>
-                    </div>
-                    <div className={style.inner_column}>
-                        <h2 className={style.subtitle}>Описание</h2>
-                        <p className={style.descr}>Вешалка в прихожую из прочного металла из серии "Ferrumon"; Металл покрыт специальной краской, которая защищает металл от коррозии продлевает срок службы; Обратите внимание, что цветопередача фото с каталога может отличаться от цветов в живом виде из-за особенностей передачи цветов монитора Вашего компьютера, ноутбука, планшета и т.д.</p>
-                    </div>
-                </div>
-            </div> 
+            {
+                catalog.map(({info}) => {
+                    return info.map(({id, title, old_price, price, image, manufacturer, material, description})=> {
+                        if(index === id) {
+                            return  <div className={style.wrapper}>
+                                    <div className={style.image}>
+                                        <img     
+                                            className={style.img}
+                                            src={image}
+                                            alt={title}
+                                        />
+                                    </div>
+                                    <div className={style.info}>
+                                        <h1 className={style.title}>{title}</h1>
+                                        <div className={classNames(style.inner, style.inner_bottom)}>
+                                            <p className={style.text}>Buyer rating:</p>
+                                            <div className={style.inner_review}>
+                                                <div className={style.inner_star}>
+                                                    <img src={star} alt="star"/>
+                                                    <img src={star} alt="star"/>
+                                                    <img src={star} alt="star"/>
+                                                    <img src={star} alt="star"/>
+                                                    <img src={star} alt="star"/>
+                                                </div>
+                                                <p className={style.text}>No reviews</p>
+                                            </div>
+                                        </div>
+                                        <div className={classNames(style.inner, style.inner_bottom)}>
+                                            <p className={style.text}>Product code:</p>
+                                            <p className={style.id_product}>{id}</p>
+                                        </div>
+                                        <div className={classNames(style.inner, style.inner_bottom)}>
+                                            <div className={classNames(style.inner_column, style.column_gap)}>
+                                                <div>
+                                                    <p className={style.last_price}>{old_price} $</p>
+                                                    <p className={style.new_price}>{price} $</p>
+                                                </div>
+                                                <div className={style.pay_part}>
+                                                    <p className={style.text}>Payment in installments:</p>
+                                                    <p className={style.text}>6 х {Math.floor(price / 6)} $</p>
+                                                </div>
+                                            </div>
+                                            <div className={classNames(style.inner_column, style.column_gap)}>
+                                                <div className={style.buy_buttons}>
+                                                    <MyButtonYellow>Add to Cart</MyButtonYellow>
+                                                    <MyButtonTab><img src={heart} alt="heart" /></MyButtonTab>
+                                                </div>
+                                                <MyButtonTab>Buy in credit</MyButtonTab>
+                                                <div className={style.icons}>
+                                                    <img src={tinkoff} alt="tinkoff"/>
+                                                    <img src={sbp} alt="sbp"/>
+                                                    <img src={gazprombank} alt="gazprombank"/>
+                                                    <img src={raiffeisen} alt="raiffeisen"/>
+                                                    <img src={citibank} alt="citibank"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className={style.inner_column}>
+                                            <h2 className={style.subtitle}>Size and Features</h2>
+                                            <div className={style.inner}>
+                                                <p className={style.text}>Housing material</p>
+                                                <p className={style.id_product}>{material}</p>
+                                            </div>
+                                            <div className={style.inner}>
+                                                <p className={style.text}>Manufacturer</p>
+                                                <p className={style.id_product}>{manufacturer}</p>
+                                            </div>
+                                        </div>
+                                        <div className={style.inner_column}>
+                                            <h2 className={style.subtitle}>Description</h2>
+                                            <p className={style.descr}>{description}</p>
+                                        </div>
+                                    </div>
+                                </div> 
+                        }
+                    })
+                })
+            }
         </div>    
     </>
             
